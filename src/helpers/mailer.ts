075da7f6a0f1e35fd10395d4bf1e626c240c74eb
dushyant kumar <dushyantvelar@gmail.com>
2026-09-07 23:27:16 +0530

    setup the mail send config by mailtrap

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,4 +1,8 @@
 import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+import User from "@/models/userModel";
+import { MailtrapTransport } from "mailtrap";
+const TOKEN = process.env.MAIL_TOKEN;
 
 interface MailProp {
   email: string;
@@ -8,25 +12,59 @@ interface MailProp {
 
 export const sendEmails = async ({ email, emailType, userId }: MailProp) => {
   try {
-    // TODO: configure mail for usage
-    const transporter = nodemailer.createTransport({
-      host: "smtp.example.com",
-      port: 587,
-      secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    const url =
+      emailType === "VERIFY"
+        ? `${process.env.DOMAIN}/verifyemail?token=${hashedToken}`
+        : `${process.env.DOMAIN}/resetpassword?token=${hashedToken}`;
+    const html = `<p> Click <a href="${url}">here</a>
+      to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}.</p>
+    <p>
+     Or copy and paste the link below into your browser:
+    </p>
+    <p>${url}</p>`;
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 360000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPassToken: hashedToken,
+        forgotPassExpiry: Date.now() + 360000,
+      });
+    }
+
+    // const transport = nodemailer.createTransport({
+    //   host: "smtp.example.com",
+    //   port: 587,
+    //   secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+    //   auth: {
+    //     user: process.env.SMTP_USER,
+    //     pass: process.env.SMTP_PASS,
+    //   },
+    // });
+    const transport = nodemailer.createTransport(
+      MailtrapTransport({
+        token: TOKEN,
+      }),
+    );
+
+    const sender = {
+      address: "hello@demomailtrap.co",
+      name: "My App",
+    };
 
     const mailOptions = {
-      from: "dushyantvelar@gmail.com", // sender address
+      from: sender, // sender address
       to: email, // list of recipients
       subject:
         emailType === "VERIFY" ? "Verify your email" : "reset your password", // subject line
-      html: "<b>Hello world?</b>", // HTML body
+      html,
     };
-    const mailResponse = await transporter.sendMail(mailOptions);
+    const mailResponse = await transport.sendMail(mailOptions);
     return mailResponse;
   } catch (err: unknown) {
     if (err instanceof Error) {
