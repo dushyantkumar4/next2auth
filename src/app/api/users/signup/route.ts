8e28024de14de88cb31b64189a4cc78941026d0c
dushyant kumar <dushyantvelar@gmail.com>
2026-09-07 21:42:03 +0530

    created the sigup route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,50 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmails } from "@/helpers/mailer";
+
+connectDB();
+interface SignupReqBody {
+  email: string;
+  password: string;
+  userName: string;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody: SignupReqBody = await request.json();
+    const { email, password, userName } = reqBody;
+    console.log(reqBody);
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "user already exist" },
+        { status: 400 },
+      );
+    }
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+    //send verification email
+    await sendEmails({ email, emailType: "VERIFY", userId: savedUser._id });
+
+    return NextResponse.json({
+      message:"User registerd successfully",
+      success:true,
+      savedUser
+    }) 
+
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+  }
+}
