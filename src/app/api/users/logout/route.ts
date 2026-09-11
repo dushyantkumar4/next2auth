993f6c87e16256dc526b091a8474561f7eb0ee5e
dushyant kumar <dushyantvelar@gmail.com>
2026-09-11 19:34:21 +0530

    feat:added the logout route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,23 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = NextResponse.json(
+      { message: "Logout Successfully", success: true },
+      { status: 200 },
+    );
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    return response;
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: "internal server error" },
+        { status: 500 },
+      );
+    }
+  }
+}
