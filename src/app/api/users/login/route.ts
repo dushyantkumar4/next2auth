d5dbed522e50bcb86065a58ef642712fa20dcfc8
dushyant kumar <dushyantvelar@gmail.com>
2026-09-11 19:34:56 +0530

    feat:added the login with route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody: LoginBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "user don't exist" },
+        { status: 404 },
+      );
+    }
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { message: "Check your credentials " },
+        { status: 400 },
+      );
+    }
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    const token = jwt.sign(
+      tokenData,
+      process.env.TOCKEN_SECRET as string,
+      { expiresIn: "1h" },
+    );
+    const response = NextResponse.json({}, { status: 200 });
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+  }
+}
